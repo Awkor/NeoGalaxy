1e844616c155ad3629fc23e1503cbff4086c94fe
Awkor <awkor.16+github@gmail.com>
2020-09-18 01:39:16 +0200

    Add borderless and fullscreen settings

--- source/menus/settings.gd
+++ source/menus/settings.gd
@@ -1,12 +1,24 @@
 extends Node
 
-onready var back_button := $PanelContainer/ButtonBack
-onready var main_menu: PackedScene = load("res://assets/scenes/menus/main.tscn")
+onready var button_back := $PanelContainer/MarginContainer/VBoxContainer/ButtonBack
+onready var check_box_fullscreen := $PanelContainer/MarginContainer/VBoxContainer/CheckBoxBorderless
+onready var check_box_borderless := $PanelContainer/MarginContainer/VBoxContainer/CheckBoxFullscreen
+onready var main_menu := load("res://assets/scenes/menus/main.tscn")
 
 
 func _ready() -> void:
-	back_button.connect("pressed", self, "_on_ButtonBack_pressed")
+	button_back.connect("pressed", self, "_on_ButtonBack_pressed")
+	check_box_borderless.connect("toggled", self, "_on_CheckBoxFullscreen_toggled")
+	check_box_fullscreen.connect("toggled", self, "_on_CheckBoxBorderless_toggled")
 
 
 func _on_ButtonBack_pressed() -> void:
 	get_tree().change_scene_to(main_menu)
+
+
+func _on_CheckBoxBorderless_toggled(toggled: bool) -> void:
+	OS.window_borderless = toggled
+
+
+func _on_CheckBoxFullscreen_toggled(toggled: bool) -> void:
+	OS.window_fullscreen = toggled
