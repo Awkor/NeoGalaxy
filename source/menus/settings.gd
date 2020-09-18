8e72d705db72f61208fafcd7d8df51536719b7ae
Awkor <awkor.16+github@gmail.com>
2020-09-18 02:18:26 +0200

    Add apply button to settings menu

--- source/menus/settings.gd
+++ source/menus/settings.gd
@@ -1,24 +1,23 @@
 extends Node
 
-onready var button_back := $PanelContainer/MarginContainer/VBoxContainer/ButtonBack
-onready var check_box_fullscreen := $PanelContainer/MarginContainer/VBoxContainer/CheckBoxBorderless
-onready var check_box_borderless := $PanelContainer/MarginContainer/VBoxContainer/CheckBoxFullscreen
+onready var button_apply := $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ButtonApply
+onready var button_back := $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ButtonBack
+onready var check_box_borderless := $PanelContainer/MarginContainer/VBoxContainer/CheckBoxBorderless
+onready var check_box_fullscreen := $PanelContainer/MarginContainer/VBoxContainer/CheckBoxFullscreen
 onready var main_menu := load("res://assets/scenes/menus/main.tscn")
 
 
 func _ready() -> void:
+	check_box_borderless.pressed = OS.window_borderless
+	check_box_fullscreen.pressed = OS.window_fullscreen
+	button_apply.connect("pressed", self, "_on_ButtonApply_pressed")
 	button_back.connect("pressed", self, "_on_ButtonBack_pressed")
-	check_box_borderless.connect("toggled", self, "_on_CheckBoxFullscreen_toggled")
-	check_box_fullscreen.connect("toggled", self, "_on_CheckBoxBorderless_toggled")
+
+
+func _on_ButtonApply_pressed() -> void:
+	OS.window_borderless = check_box_borderless.pressed
+	OS.window_fullscreen = check_box_fullscreen.pressed
 
 
 func _on_ButtonBack_pressed() -> void:
 	get_tree().change_scene_to(main_menu)
-
-
-func _on_CheckBoxBorderless_toggled(toggled: bool) -> void:
-	OS.window_borderless = toggled
-
-
-func _on_CheckBoxFullscreen_toggled(toggled: bool) -> void:
-	OS.window_fullscreen = toggled
