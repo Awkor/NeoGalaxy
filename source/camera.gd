0dbd0dc3ccdb0ff5bfd32cc70b68ab08eca2cc16
Awkor <awkor.16+github@gmail.com>
2020-09-20 04:02:50 +0200

    Add camera smoothing

--- source/camera.gd
+++ source/camera.gd
@@ -1,46 +1,72 @@
 extends Spatial
 
 var rotate := false
-var rotation_scale := 0.005
+var rotation_scale := 0.03
+var rotation_smooth := true
+var rotation_stiffness := 10.0
 var rotation_vertical_limit := PI / 2
 var rotation_vertical_limited := true
 
-var zoom_in := false
-var zoom_out := false
-var zoom_amount := 0.1
-var zoom_direction := 0
+var target: Spatial = null
+
+var zoom_maximum := 512.0
+var zoom_minimum := 1.0
+var zoom_scale := 0.5
+var zoom_smooth := true
+var zoom_stiffness := 10.0
 
 onready var spring_arm: SpringArm = $SpringArm
+onready var spring_arm_length_target := spring_arm.spring_length
+
+onready var rotation_angle_target_horizontal := spring_arm.rotation.x
+onready var rotation_angle_target_vertical := rotation.y
 
 
-func _unhandled_input(event) -> void:
-	zoom_in = false
-	zoom_out = false
-	zoom_direction = 0
+func _unhandled_input(event: InputEvent) -> void:
+	var zoom_in := false
+	var zoom_out := false
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT:
-			rotate = event.pressed
-		zoom_in = event.button_index == BUTTON_WHEEL_UP
-		zoom_out = event.button_index == BUTTON_WHEEL_DOWN
+		rotate = event.button_index == BUTTON_LEFT and event.pressed
+		zoom_in = event.button_index == BUTTON_WHEEL_DOWN
+		zoom_out = event.button_index == BUTTON_WHEEL_UP
 	elif event is InputEventMouseMotion:
 		if rotate:
-			var angle_horizontal: float = -event.relative.x * rotation_scale
-			var angle_vertical: float = -event.relative.y * rotation_scale
-			_rotate_horizontally(angle_horizontal)
-			_rotate_vertically(angle_vertical)
+			var rotation_angle_change_horizontal: float = -event.relative.x * rotation_scale
+			var rotation_angle_change_vertical: float = -event.relative.y * rotation_scale
+			rotation_angle_target_horizontal = rotation.y + rotation_angle_change_horizontal
+			rotation_angle_target_vertical = spring_arm.rotation.x + rotation_angle_change_vertical
 	if zoom_in or zoom_out:
-		zoom_direction = 1 if zoom_in else -1
-		spring_arm.spring_length += zoom_amount * zoom_direction
+		var zoom_direction := 1 if zoom_in else -1
+		var spring_arm_length_change := spring_arm.spring_length * zoom_scale * zoom_direction
+		spring_arm_length_target = spring_arm.spring_length + spring_arm_length_change
 
 
-func _rotate_horizontally(angle: float) -> void:
-	rotate_y(angle)
+func _process(delta: float) -> void:
+	transform.origin = target.transform.origin
+	_rotate_horizontally(rotation_angle_target_horizontal, delta)
+	_rotate_vertically(rotation_angle_target_vertical, delta)
+	_zoom(spring_arm_length_target, delta)
 
 
-func _rotate_vertically(angle: float) -> void:
+func _rotate_horizontally(target: float, delta: float) -> void:
+	if rotation_smooth:
+		var weight := delta * rotation_stiffness
+		target = lerp_angle(rotation.y, target, weight)
+	rotation.y = target
+
+
+func _rotate_vertically(target: float, delta: float) -> void:
 	if rotation_vertical_limited:
-		var predicted := spring_arm.rotation.x + angle
-		if abs(predicted) > rotation_vertical_limit:
-			var excess := abs(predicted) - rotation_vertical_limit
-			angle -= excess * sign(predicted)
-	spring_arm.rotate_x(angle)
+		target = clamp(target, -rotation_vertical_limit, rotation_vertical_limit)
+	if rotation_smooth:
+		var weight := delta * rotation_stiffness
+		target = lerp_angle(spring_arm.rotation.x, target, weight)
+	spring_arm.rotation.x = target
+
+
+func _zoom(target: float, delta: float) -> void:
+	target = clamp(target, zoom_minimum, zoom_maximum)
+	if zoom_smooth:
+		var weight := delta * zoom_stiffness
+		target = lerp(spring_arm.spring_length, target, weight)
+	spring_arm.spring_length = target
