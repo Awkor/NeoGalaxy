a6ba40f06067dee48919e13537207dd6b99c88be
Awkor <awkor.16+github@gmail.com>
2020-09-26 01:44:11 +0200

    Add FOV control to camera

--- source/camera.gd
+++ source/camera.gd
@@ -1,5 +1,12 @@
 extends Spatial
 
+var fov_change := 2.0
+var fov_default := 70.0
+var fov_maximum := 90.0
+var fov_minimum := 50.0
+var fov_smooth := true
+var fov_stiffness := 10.0
+
 var rotate := false
 var rotation_scale := 0.03
 var rotation_smooth := true
@@ -15,6 +22,9 @@ var zoom_scale := 0.5
 var zoom_smooth := true
 var zoom_stiffness := 10.0
 
+onready var camera: Camera = $SpringArm/Camera
+onready var camera_fov_target := camera.fov
+
 onready var spring_arm: SpringArm = $SpringArm
 onready var spring_arm_length_target := spring_arm.spring_length
 
@@ -28,8 +38,13 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			rotate = event.pressed
-		zoom_in = event.button_index == BUTTON_WHEEL_DOWN
-		zoom_out = event.button_index == BUTTON_WHEEL_UP
+		if Input.is_key_pressed(KEY_ALT):
+			camera_fov_target += fov_change if event.button_index == BUTTON_WHEEL_UP else 0
+			camera_fov_target -= fov_change if event.button_index == BUTTON_WHEEL_DOWN else 0
+			camera_fov_target = clamp(camera_fov_target, fov_minimum, fov_maximum)
+		else:
+			zoom_in = event.button_index == BUTTON_WHEEL_DOWN
+			zoom_out = event.button_index == BUTTON_WHEEL_UP
 	elif event is InputEventMouseMotion:
 		if rotate:
 			var rotation_angle_change_horizontal: float = -event.relative.x * rotation_scale
@@ -46,6 +61,7 @@ func _process(delta: float) -> void:
 	transform.origin = target.transform.origin
 	_rotate_horizontally(rotation_angle_target_horizontal, delta)
 	_rotate_vertically(rotation_angle_target_vertical, delta)
+	_update_fov(camera_fov_target, delta)
 	_zoom(spring_arm_length_target, delta)
 
 
@@ -63,6 +79,12 @@ func _rotate_vertically(target: float, delta: float) -> void:
 		var weight := delta * rotation_stiffness
 		target = lerp_angle(spring_arm.rotation.x, target, weight)
 	spring_arm.rotation.x = target
+
+
+func _update_fov(target: float, delta: float) -> void:
+	if fov_smooth:
+		target = lerp(camera.fov, target, delta * fov_stiffness)
+	camera.fov = target
 
 
 func _zoom(target: float, delta: float) -> void:
