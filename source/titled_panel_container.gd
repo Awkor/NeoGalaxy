5875b22e271038edf9c921ed43b4f90d728670d2
Awkor <awkor.16+github@gmail.com>
2020-10-08 16:39:59 +0200

    Export content and title margin in titled panel

--- source/titled_panel_container.gd
+++ source/titled_panel_container.gd
@@ -1,15 +1,15 @@
 class_name TitledPanelContainer, "res://assets/icons/titled_panel_container.svg"
 extends PanelContainer
 
+export (int) var content_margin = 4
 export (String) var title
+export (int) var title_margin = 6
 
 var margin_container_bottom := MarginContainer.new()
-var margin_container_bottom_margin := 4
 var margin_container_top := MarginContainer.new()
 
 var title_background := ColorRect.new()
 var title_label := Label.new()
-var title_margin := 8
 var title_margin_container := MarginContainer.new()
 
 var style: StyleBoxFlat = get_stylebox("panel", "PanelContainer")
@@ -19,10 +19,10 @@ var v_box_container_separation := 0
 
 
 func _ready():
-	margin_container_bottom.set("custom_constants/margin_bottom", margin_container_bottom_margin)
-	margin_container_bottom.set("custom_constants/margin_left", margin_container_bottom_margin)
-	margin_container_bottom.set("custom_constants/margin_right", margin_container_bottom_margin)
-	margin_container_bottom.set("custom_constants/margin_top", margin_container_bottom_margin)
+	margin_container_bottom.set("custom_constants/margin_bottom", content_margin)
+	margin_container_bottom.set("custom_constants/margin_left", content_margin)
+	margin_container_bottom.set("custom_constants/margin_right", content_margin)
+	margin_container_bottom.set("custom_constants/margin_top", content_margin)
 	title_background.color = style.border_color
 	title_label.size_flags_horizontal |= SIZE_EXPAND
 	title_label.text = title
