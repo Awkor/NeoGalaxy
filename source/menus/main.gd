30555ea535c319af356d7c7b0121834fbd2ece70
Awkor <awkor.16+github@gmail.com>
2020-10-12 03:53:42 +0200

    Update main menu

--- source/menus/main.gd
+++ source/menus/main.gd
@@ -1,22 +1,31 @@
 extends Node
 
+export (NodePath) var path_button_play
 export (NodePath) var path_button_quit
 export (NodePath) var path_button_settings
 
+onready var button_play := get_node(path_button_play)
 onready var button_quit := get_node(path_button_quit)
 onready var button_settings := get_node(path_button_settings)
 
-onready var settings_menu: PackedScene = load("res://scenes/menus/settings.tscn")
+onready var settings_menu: PackedScene = preload("res://scenes/menus/settings.tscn")
+onready var scene: PackedScene = load("res://scenes/tests/chunk_grid.tscn")
+onready var tree := get_tree()
 
 
 func _ready() -> void:
+	button_play.connect("pressed", self, "_on_ButtonPlay_pressed")
 	button_quit.connect("pressed", self, "_on_ButtonQuit_pressed")
 	button_settings.connect("pressed", self, "_on_ButtonSettings_pressed")
 
 
+func _on_ButtonPlay_pressed() -> void:
+	tree.change_scene_to(scene)
+
+
 func _on_ButtonQuit_pressed() -> void:
-	get_tree().quit()
+	tree.quit()
 
 
 func _on_ButtonSettings_pressed() -> void:
-	get_tree().change_scene_to(settings_menu)
+	tree.change_scene_to(settings_menu)
